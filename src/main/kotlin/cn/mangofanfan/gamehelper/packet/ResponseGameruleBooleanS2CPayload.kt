1917fd8c0d11a9be6e100020bb3e7c7a605164e3
MangoFanFan_ <mangofanfanw@icloud.com>
2025-11-25 16:38:29 +0800

    增加文档

--- src/main/kotlin/cn/mangofanfan/gamehelper/packet/ResponseGameruleBooleanS2CPayload.kt
+++ src/main/kotlin/cn/mangofanfan/gamehelper/packet/ResponseGameruleBooleanS2CPayload.kt
@@ -7,9 +7,17 @@ import net.minecraft.network.packet.CustomPayload
 import net.minecraft.util.Identifier
 
 /**
- * 从客户端向服务端发送的数据包，请求游戏规则`gamerule`的值。布尔值类型
+ * 从服务端向客户端发送的数据包，回复客户端对于`gameruleName`游戏规则的请求。
  *
  * **在单人游戏中不应该处理此数据包。**
+ *
+ * 由于Minecraft的游戏规则有两种值的类型，因此回复游戏规则的数据包也有两种：
+ * * [cn.mangofanfan.gamehelper.packet.ResponseGameruleBooleanS2CPayload] 用于 [Boolean]
+ * * [cn.mangofanfan.gamehelper.packet.ResponseGameruleIntS2CPayload] 用于 [Int]
+ *
+ * @param gameruleName 回复的游戏规则的名称，[String]
+ * @param value 游戏规则的值，本数据包需要为[Boolean]
+ * @param translationKey 游戏规则的翻译键，用于在客户端中显示，[String]
  */
 class ResponseGameruleBooleanS2CPayload(val gameruleName: String, val value: Boolean, val translationKey: String) : CustomPayload {
     override fun getId(): CustomPayload.Id<out CustomPayload?> {
